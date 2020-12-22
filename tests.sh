9f70c2881be6a65962ef1fa92a0a2cc89461e070
Korben Dallas <pvsaintpe@icloud.com>
2020-12-23 00:16:04 +0300

    #patch: minor fixes in workflows

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 composer lint-fix
-php -d pcov.directory='.' vendor/bin/phpunit --coverage-html build
+php -d pcov.directory='.' vendor/bin/phpunit --coverage-html build --coverage-text
 
